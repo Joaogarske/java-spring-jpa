2c8f82f441a8010fa68d12aaab84c64cf85ed7bb
joao roberto <jrgarske5@gmail.com>
2024-09-04 17:59:40 -0300

    entidade user criada e resource criado

---
+++ src/main/java/com/projetojoao/proj/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.projetojoao.proj.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetojoao.proj.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"joao","joao@gmail.com","999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
